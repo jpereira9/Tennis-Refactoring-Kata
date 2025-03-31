bd978efb49a141510028e05f4d8937107f78dc66
jpereira9 <jaimepereiraalvarez@gmail.com>
2025-03-31 15:23:25 +0200

    refactor TennisGame1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,73 +1,69 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
     private int player1Score = 0;
     private int player2Score = 0;
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1") {
+        if (Objects.equals(playerName, "player1")) {
             player1Score += 1;
         } else
             player2Score += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
+        StringBuilder score = new StringBuilder();
         if (player1Score == player2Score) {
-            switch (player1Score) {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-            }
-        }
-        else if (player1Score >= 4 || player2Score >= 4) {
-            int minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult>= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = getScoreIfEqualPoints();
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            score = getScoreIfFourPoints();
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) {
-                    tempScore = player1Score;
-                }
-                else {
-                    score += "-";
-                    tempScore = player2Score;
-                }
-                switch(tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            getScoreIfDifferenceInPoints(score);
+        }
+        return score.toString();
+    }
+
+    private void getScoreIfDifferenceInPoints(StringBuilder score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            }
+            else {
+                score.append("-");
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
+                case 0 -> score.append("Love");
+                case 1 -> score.append("Fifteen");
+                case 2 -> score.append("Thirty");
+                case 3 -> score.append("Forty");
             }
         }
+    }
+
+    private StringBuilder getScoreIfFourPoints() {
+        StringBuilder score;
+        int player1ScoreMinusPlayer2Score = player1Score - player2Score;
+        if (player1ScoreMinusPlayer2Score == 1) score = new StringBuilder("Advantage player1");
+        else if (player1ScoreMinusPlayer2Score == -1) score = new StringBuilder("Advantage player2");
+        else if (player1ScoreMinusPlayer2Score>= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+        return score;
+    }
+
+    private StringBuilder getScoreIfEqualPoints() {
+        StringBuilder score;
+        score = new StringBuilder(switch (player1Score) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
+        });
         return score;
     }
 }
